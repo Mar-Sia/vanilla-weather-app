5c9fc0f099835a153c9d23f26d308894d4cfe89a
Mar-Sia <98830671+Mar-Sia@users.noreply.github.com>
2022-03-22 17:36:02 +0000

    Added current time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  let currentDate = document.querySelector("#current-date");
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#degrees");
   let temperature = Math.round(response.data.main.temp);
@@ -8,7 +34,6 @@ function displayTemperature(response) {
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
-  console.log(response.data);
 
   let humidityElement = document.querySelector("#humidity");
 
@@ -17,6 +42,9 @@ function displayTemperature(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let windSpeed = Math.round(response.data.wind.speed);
   windSpeedElement.innerHTML = `${windSpeed} mph`;
+
+  let dateElement = document.querySelector("#current-date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "309df4d5a54300eab011fb0dc95d4919";

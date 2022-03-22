79a5c978dae2231d2e0171abceaa1b9c7b81f566
Mar-Sia <98830671+Mar-Sia@users.noreply.github.com>
2022-03-22 17:14:44 +0000

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#degrees");
+  let temperature = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = `${temperature}°`;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
   console.log(response.data);
+
+  let humidityElement = document.querySelector("#humidity");
+
+  humidityElement.innerHTML = `${response.data.main.humidity} %`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeed = Math.round(response.data.wind.speed);
+  windSpeedElement.innerHTML = `${windSpeed} mph`;
 }
 
 let apiKey = "309df4d5a54300eab011fb0dc95d4919";

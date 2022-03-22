fc6f3f2b5b0953904193ae05ecf90110c4c2ba6a
Mar-Sia <98830671+Mar-Sia@users.noreply.github.com>
2022-03-22 16:39:02 +0000

    Added more styling in CSS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "309df4d5a54300eab011fb0dc95d4919";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);

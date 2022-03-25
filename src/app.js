2739df47c21592243e4ef579a8dce79b062c9cd1
Mar-Sia <98830671+Mar-Sia@users.noreply.github.com>
2022-03-25 11:22:09 +0000

    Added the sunrise and sunset functions & elements

--- src/app.js
+++ src/app.js
@@ -24,6 +24,36 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function formatSunrise(sunrise) {
+  let date = new Date(sunrise);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let sunriseTime = document.querySelector("#sunrise");
+  return `${hours}:${minutes}`;
+}
+
+function formatSunset(sunset) {
+  let date = new Date(sunset);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let ssunsetTime = document.querySelector("#sunset");
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#degrees");
   let temperature = Math.round(response.data.main.temp);
@@ -45,9 +75,16 @@ function displayTemperature(response) {
 
   let dateElement = document.querySelector("#current-date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let sunriseElement = document.querySelector("#sunrise");
+  sunriseElement.innerHTML = formatSunrise(response.data.sys.sunrise * 1000);
+
+  let sunsetElement = document.querySelector("#sunset");
+  sunsetElement.innerHTML = formatSunset(response.data.sys.sunset * 1000);
 }
 
 let apiKey = "309df4d5a54300eab011fb0dc95d4919";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=${apiKey}&units=metric`;
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
